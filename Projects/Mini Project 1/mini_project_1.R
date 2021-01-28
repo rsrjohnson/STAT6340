72ecfd4e11e2d5d30b5aa91784c31b90c5bfab57
Randy Suarez Rodes <rsrjohnson@gmail.com>
2021-01-27 22:49:13 -0600

    Add contour plot draft and update assignment pdf

--- Projects/Mini Project 1/mini_project_1.R
+++ Projects/Mini Project 1/mini_project_1.R
@@ -3,11 +3,14 @@ setwd(dirname(getActiveDocumentContext()$path))
 #Packages, data, functions and Global Variables
 
 library(ggplot2) #Used for graphics an visual representations
-library(class) #Used for knn models
+library(class) #Used for KNN models
 
-training_error_rate=function(ypred,ytrue,n)
+set.seed(8467) #Fixing a seed to replicate results in case of a tie on KNN
+
+classification_error_rate=function(ypred,ytrue)
 {
-  sum(ypred!=ytrue)/n
+  a=mean(ypred!=ytrue)
+ 
 }
 
 
@@ -41,11 +44,11 @@ Error_df=data.frame(k=seq(1,topK),k_rate=1/seq(1,topK),trn_Error=rep(0,topK),tst
 for(i in 1:topK)
 {
   trn_pred=knn(trn,trn,cl=trn_y,k=i)
-  Error_df$trn_Error[i]=training_error_rate(trn_pred,trn_y,n_trn)
+  Error_df$trn_Error[i]=classification_error_rate(trn_pred,trn_y)
   
   
   tst_pred=knn(trn,tst,cl=trn_y,k=i)
-  Error_df$tst_Error[i]=training_error_rate(tst_pred,tst_y,n_tst)
+  Error_df$tst_Error[i]=classification_error_rate(tst_pred,tst_y)
   
 }
 
@@ -53,3 +56,33 @@ for(i in 1:topK)
 g1=ggplot(data=Error_df, aes(x=k_rate,y=trn_Error))
 g1 +   geom_line(aes(y = trn_Error), color = "blue")+
    geom_line(aes(y = tst_Error), color="orange")
+
+g1
+
+g2=ggplot(data=Error_df, aes(x=k,y=trn_Error))
+g2 +   geom_line(aes(y = trn_Error), color = "blue")+
+  geom_line(aes(y = tst_Error), color="orange")
+
+g2
+
+#Question 1.c
+
+ind_optimalK=which.min(Error_df$tst_Error)
+optimalK=Error_df$k[ind_optimalK]
+optK_trnError=Error_df$trn_Error[ind_optimalK]
+optK_tstError=Error_df$tst_Error[ind_optimalK]
+
+
+#Question 1.d
+
+bestK=knn(trn,trn,cl=trn_y,k=optimalK,prob = T )
+prob <- attr(bestK, "prob")
+prob = ifelse(prob=="1", prob, 1-prob)
+x1 <- 1:10
+x2 <- 1:100
+prob_matrix = matrix(prob, length(x1), length(x2))
+contour(x1,x2,prob_matrix,levels=0.5, labels="", xlab="", ylab="", main=
+          "Some Picture", lwd=2, axes=FALSE)
+gd <- expand.grid(x=x1, y=x2)
+points(gd, pch="o", cex=1.2, col=ifelse(prob==1, "blue", "orange"))
+box()
