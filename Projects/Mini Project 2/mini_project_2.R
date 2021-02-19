077c107cc24fff9129deafacda9bcf1da4489a22
Randy Suarez Rodes <rsrjohnson@gmail.com>
2021-02-18 23:44:39 -0600

    Add question 2 and 3, graph improved, start report

--- Projects/Mini Project 2/mini_project_2.R
+++ Projects/Mini Project 2/mini_project_2.R
@@ -3,12 +3,14 @@ library(ggplot2) #Used for graphics and visual representations
 library(GGally) #Visualization of pairs
 library(ggpubr) #Graphics handling
 library(ggcorrplot) #Visualization of correlations
-library(fastDummies) #One hot encoding
-library(MASS) #Used for lda and qda analysis
+
+library(fastDummies) #One hot encoding categorical data
+library(MASS) #Used for ld and qd analysis
+library(pROC) #Used to obtain ROC curve and find cutoff
+library(caret) #Confusion matrix and classification metrics
 
 
-
-rdseed=8467 #Seed to replicate results in case of a tie on KNN
+rdseed=8467 #Seed to replicate results in case of a tie on LDA or QDA
 
 #Experiment 1
 
@@ -24,25 +26,26 @@ wine$Region=as.factor(wine$Region)
 
 #Exploring correlations
 
-#we want now to visualize correlations. 
-#Since regions is a factor variable, to be able to establish correlations
-#we need to one hot encode the data. We proceed to leave one out hot encode
-#a copy of wine for this purpose
+#We want now to visualize correlations. 
+#Since regions is a factor variable, to be able to establish correlations with it
+#we need to one hot encode the data. We proceed to leave one class out hot encode Region
+#we manipulate a copy of wine for this purpose
 
 wine_std=wine
-wine_std[,1:6]=scale(wine_std[,1:6])
+#wine_std[,1:6]=scale(wine_std[,1:6])
 
 wine_dummy=wine
 wine_dummy=dummy_cols(wine_dummy, select_columns = "Region",remove_first_dummy = TRUE)
-wine_dummy$Region=NULL
+wine_dummy$Region=NULL #dropping the Region factors
 
+#Placing Quality as last variable
 temp=wine_dummy$Quality
 wine_dummy$Quality=NULL
 wine_dummy$Quality=temp
 
+#Finding correlation matrix
 corr1 = round(cor(wine_dummy), 2)
 
-#ggcorr(wine_dummy,label = TRUE,label_round = 2)
 
 ggcorrplot(corr1,lab=TRUE,type = "full")
 #Visualizing the correlation matrix we can see that predictors Oakiness and Clarity have a very low correlation with Quality
@@ -53,12 +56,13 @@ ggcorrplot(corr1,lab=TRUE,type = "full")
 #Also variable Body presents a positive correlation not as high as other predictors, 
 #therefore we required further analysis to determine the relevance of this predictor for the response variable
 
-#Another point to notice that predictors Aroma and Body are are highly with Flavor, this can cause overfitting issues, 
-#so with further analysis one or some of this predictors might be dropped since they can be explained by the other predictor.
+#Another point to notice it that predictors Aroma, Body, Flavor and Region are highly correlated. This can cause overfitting issues, 
+#so with further analysis one or some of this predictors might be dropped since they can be explained by the others.
 
-ggpairs(wine[,1:6], upper=list(continuous="points"),axisLabels="internal")
+ggpairs(wine[,1:6], upper=list(continuous="cor"),axisLabels="internal")
 #As expected from the correlation matrix we can notice that the Predictors Aroma, Body and Flavor exhibit a positive linear relation with Quality. 
 
+g_region=ggplot(wine,aes(Region,Quality,fill=Region))+geom_boxplot()
 
 
 #Question 1.b
@@ -161,7 +165,7 @@ ggarrange(g_Aroma, g_Body, g_Flavor,
 m_Full=lm(Quality~.,data=wine)
 summary(m_Full)
 
-#We can observe that we can reject the null hypothesis  for the Flavor and Region predictors.
+#We can observe that we can reject the null hypothesis for the Flavor and Region predictors.
 #We will proceed to drop one variable at a time and recheck the p-values.
 
 #Question 1.e
@@ -178,11 +182,11 @@ summary(m3)
 m4=lm(Quality~.-Clarity-Body-Aroma-Oakiness,data=wine) #dropping Clarity, Body, Aroma and Oakiness
 summary(m4)
 
-#Finally on m4 all predictors all predictors are statistically significant for the response variable
+#Finally on m4 all predictors all are statistically significant for the response variable
 
 anova(m4,m_Full)
 
-#The anova test confirm our findings, with a p-value of 0.6528, we fail to reject the null hypothesis, this is all additional predictors are 0.
+#The anova test confirms our findings. With a p-value of 0.6528, we fail to reject the null hypothesis, this is all additional predictors are 0.
 
 
 #Exploring interactions
@@ -207,12 +211,8 @@ anova(m_final,m_final2)
 #Question 1.g
 
 
-
-
-
-
 k=which(wine$Region=="1")
-predict1=fitted(m_final)[k]
+predict1=m_final$fitted.values[k]
 
 
 #ggplot(wine,aes(x=Flavor,y=Quality,color=Region))+geom_point()+geom_line(aes(x=wine$Flavor[k],y=predict1),data=wine[k,])
@@ -224,7 +224,7 @@ ggplot(wine,aes(x=Flavor,y=Quality,color=Region))+geom_point()+geom_abline(slope
 predict (m_final ,wine[k,], se.fit = TRUE,
          interval ="prediction")
 
-#95% prediction intervals
+#95% confidence intervals
 predict (m_final ,wine[k,], se.fit = TRUE,
          interval ="confidence")
 
@@ -232,7 +232,7 @@ predict (m_final ,wine[k,], se.fit = TRUE,
 predict (m_final ,data.frame(Flavor=mean(wine$Flavor),Region=as.factor(1)),
          interval ="prediction")
 
-#95% prediction intervals
+#95% confidence intervals
 predict (m_final ,wine[k,], se.fit = TRUE,
          interval ="confidence")
 
@@ -265,9 +265,9 @@ adm.train=rbind(admission[k1,][-(1:5),],admission[k2,][-(1:5),],admission[k3,][-
 
 #Question 2.a
 g=ggplot(adm.train,aes(x=GPA,y=GMAT,color=Group))+geom_point()
-g_GPA=ggplot(adm.train,(aes(Group,GPA)))+geom_boxplot()
-g_GMAT=ggplot(adm.train,(aes(Group,GMAT)))+geom_boxplot()
-ggarrange(g,g_GPA, g_GMAT,
+g_GPA=ggplot(adm.train,aes(Group,GPA,fill=Group))+geom_boxplot()
+g_GMAT=ggplot(adm.train,aes(Group,GMAT,fill=Group))+geom_boxplot()
+ggarrange(g+theme(legend.position = "none"),g_GPA, g_GMAT,
           labels = c("GMAT vs GPA","Boxplot GPA", "Boxplot GMAT"),
           ncol = 3, nrow = 1)
 
@@ -387,12 +387,93 @@ ggarrange(g_train, g_test,
 
 #Experiment 3
 
-diabetes <- read.csv("diabetes.csv")
+
+diabetes = read.csv("diabetes.csv")
+
+names(diabetes)=c("Pregnancies", "Glucose", "BloodPressure", "SkinThickness",
+                  "Insulin",   "BMI",  "DiabetesPedigreeFunction", "Age","Outcome" )
+
+diabetes$Outcome=as.factor(diabetes$Outcome)
+
+
 
 #Question 3.a
 
+table(diabetes$Outcome)
+
+g1=ggplot(diabetes,aes(Outcome,Pregnancies,fill=Outcome))+geom_boxplot()
+g2=ggplot(diabetes,aes(Outcome,Glucose,fill=Outcome))+geom_boxplot()
+g3=ggplot(diabetes,aes(Outcome,BloodPressure,fill=Outcome))+geom_boxplot()
+g4=ggplot(diabetes,aes(Outcome,SkinThickness,fill=Outcome))+geom_boxplot()
+ggarrange(g1, g2, g3, g4, ncol = 2, nrow = 2)
+
+g5=ggplot(diabetes,aes(Outcome,Insulin,fill=Outcome))+geom_boxplot()
+g6=ggplot(diabetes,aes(Outcome,BMI,fill=Outcome))+geom_boxplot()
+g7=ggplot(diabetes,aes(Outcome,DiabetesPedigreeFunction,fill=Outcome))+geom_boxplot()
+g8=ggplot(diabetes,aes(Outcome,Age,fill=Outcome))+geom_boxplot()
+ggarrange(g5, g6, g7, g8, ncol = 2, nrow = 2)
+
+ggplot(diabetes,aes(x=Glucose,y=Age,color=Outcome))+geom_point()
+
+
+
+actual=diabetes$Outcome
+
 #Question 3.b
+
+m_diabetes_lda=lda(Outcome~., data=diabetes)
+
+
+predicted_lda=predict(m_diabetes_lda,diabetes)
+
+CM1=confusionMatrix(predicted_lda$class, actual, positive="1")
+CM1$table
+1-CM1$overall[["Accuracy"]]
+CM1$byClass[["Sensitivity"]]
+CM1$byClass[["Specificity"]]
+
+roc.lda = roc(diabetes$Outcome, predicted_lda$posterior[, "1"], levels = c("0", "1"),direction="<")
+
+ggroc(roc.lda,color = "#F8766D"  ,legacy.axes = TRUE)+
+  geom_segment(aes(x = 0, xend = 1, y = 0, yend = 1), color="grey", linetype="dashed")+
+  ggtitle("Linear Discriminant Analysis")
 
 #Question 3.c
 
+m_diabetes_qda=qda(Outcome~., data=diabetes)
+
+predicted_qda=predict(m_diabetes_qda,diabetes)
+
+CM2=confusionMatrix(predicted_qda$class, actual, positive="1")
+CM2$table
+1-CM2$overall[["Accuracy"]]
+CM2$byClass[["Sensitivity"]]
+CM2$byClass[["Specificity"]]
+
+roc.qda = roc(diabetes$Outcome, predicted_qda$posterior[, "1"], levels = c("0", "1"),direction="<")
+
+ggroc(roc.qda, color="#00BFC4",legacy.axes = TRUE)+
+  geom_segment(aes(x = 0, xend = 1, y = 0, yend = 1), color="grey", linetype="dashed")+
+  ggtitle("Quadratic Discriminant Analysis")
+
 #Question 3.d
+
+
+ggroc(list(lda=roc.lda,qda=roc.qda),legacy.axes = TRUE)+
+  geom_segment(aes(x = 0, xend = 1, y = 0, yend = 1), color="grey", linetype="dashed")+
+  annotate("text", x = 0.5, y = 0.83, vjust = 0, label = paste("AUC =",sprintf("%.4f",roc.lda$auc)),color="#F8766D")+
+  annotate("text", x = 0.5, y = 0.78, vjust = 0, label = paste("AUC =",sprintf("%.4f",roc.qda$auc)),color="#00BFC4")
+
+
+
+roc.lda$auc
+roc.qda$auc
+
+cut.lda=coords(roc.lda,"best")
+cut.qda=coords(roc.qda,"best")
+
+
+pred_newcutoff=ifelse(predicted_lda$posterior[,2]>=cut.lda[1,1],"1","0")
+pred_newcutoff=as.factor(pred_newcutoff)
+CM3=confusionMatrix(pred_newcutoff, actual, positive="1")
+CM3$table
