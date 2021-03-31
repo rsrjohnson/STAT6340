761d35379337672e119ea278a82aa1cd7da20e7a
Randy Suarez Rodes <rsrjohnson@gmail.com>
2021-03-30 23:25:05 -0500

    Update project instructions, edit comments and graphics

--- Projects/Mini Project 4/mini_project_4.R
+++ Projects/Mini Project 4/mini_project_4.R
@@ -1,15 +1,12 @@
 #Packages
 library(ggplot2) #Used for graphics and visual representations
 
-library(ggpubr) #Used for graphics handling
 library(MASS) #Used for LD and QD analysis
 library(caret) #Used to handle LOOCV training
-library(boot) #Used for bootstrapping
 
-library(leaps) #Used for best-subset selection and 
-library(bestglm)
-library(glmnet)
-library(tidyverse)
+library(leaps) #Used for best-subset and forward and backward stepwise selection with linear models 
+library(bestglm) #Used for best-subset and forward and backward stepwise selection with generalized linear models 
+library(glmnet) #Use for Ridge Regression and Lasso
 library(broom) #To create tidy objects for ggplot visualization
 
 rdseed=8466 #Seed to replicate results
@@ -70,13 +67,13 @@ m.subset.summ=summary(m.subset)
 kbest=which.max(m.subset.summ$adjr2)
 
 #Visualizing adj r2 vs number of predictors
-ggplot(data.frame(predictors = 1:totpred, adj_R2 = m.subset.summ$adjr2),
+print(ggplot(data.frame(predictors = 1:totpred, adj_R2 = m.subset.summ$adjr2),
        aes(x=predictors,y=adj_R2))+geom_line(size=1)+
   geom_point(aes(x=kbest,y=m.subset.summ$adjr2[kbest]),
              color="red",size=4,shape=20)+
   labs(x="Number of Variables",y="Adjusted RSq")+
   theme(plot.title=element_text(hjust=0.5))+
-  ggtitle("Best Subset Selection Adjusted RSq")
+  ggtitle("Best Subset Selection Adjusted RSq"))
 
 #Exploring the coefficients of the best model
 selected_predictors=coef(m.subset, kbest)
@@ -111,13 +108,13 @@ m.forward.summ=summary(m.forward)
 k.forward=which.max(m.forward.summ$adjr2)
 
 #Visualizing adj r2 vs number of predictors
-ggplot(data.frame(predictors = 1:totpred, adj_R2 = m.forward.summ$adjr2),
+print(ggplot(data.frame(predictors = 1:totpred, adj_R2 = m.forward.summ$adjr2),
        aes(x=predictors,y=adj_R2))+geom_line(size=1)+
   geom_point(aes(x=k.forward,y=m.forward.summ$adjr2[k.forward]),
              color="red",size=4,shape=20)+
   labs(x="Number of Variables",y="Adjusted RSq")+
   theme(plot.title=element_text(hjust=0.5))+
-  ggtitle("Forward Stepwise Selection Adjusted RSq")
+  ggtitle("Forward Stepwise Selection Adjusted RSq"))
 
 #Exploring the coefficients of the best model
 selected_predictors=coef(m.forward, k.forward)
@@ -141,13 +138,13 @@ m.backward.summ=summary(m.backward)
 k.backward=which.max(m.backward.summ$adjr2)
 
 #Visualizing adj r2 vs number of predictors
-ggplot(data.frame(predictors = 1:totpred, adj_R2 = m.backward.summ$adjr2),
+print(ggplot(data.frame(predictors = 1:totpred, adj_R2 = m.backward.summ$adjr2),
        aes(x=predictors,y=adj_R2))+geom_line(size=1)+
   geom_point(aes(x=k.backward,y=m.backward.summ$adjr2[k.backward]),
              color="red",size=4,shape=20)+
   labs(x="Number of Variables",y="Adjusted RSq")+
   theme(plot.title=element_text(hjust=0.5))+
-  ggtitle("Backward Stepwise Selection Adjusted RSq")
+  ggtitle("Backward Stepwise Selection Adjusted RSq"))
 
 #Exploring the coefficients of the best model
 selected_predictors=coef(m.backward, k.backward)
@@ -165,17 +162,18 @@ df.coeff.estimates[4,-(2:4)]=coeff.subset
 #Defining predictors, response and lambdas
 response=wine[,6]
 predictors=model.matrix(Quality ~ ., wine)[, -1]
-lambdas = 10^seq(10, -2, length = 100)
+lambdas = 10^seq(10, -3, length = 100)
 
 
-#Visualizing coefficients values
 m.ridge = glmnet(predictors, response, alpha = 0, lambda = lambdas)
+
+#Visualizing coefficients values vs lambdas
 plot(m.ridge, xvar = "lambda")
 
 #Applying LOOCV to find best lambda
 cv.ridge = cv.glmnet(predictors, response, alpha = 0, nfolds = n, lambda = lambdas, grouped = FALSE, type.measure = "mse")
 
-#Tidy data frames to graph our model
+#Tidy data frames to graph
 tidy_cv <- tidy(cv.ridge)
 glance_cv <- glance(cv.ridge)
 
@@ -185,12 +183,13 @@ g.ridge = ggplot(tidy_cv, aes(lambda, estimate)) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .25)+
   geom_vline(xintercept = glance_cv$lambda.min) +
   geom_vline(xintercept = glance_cv$lambda.1se, lty = 2)+
+  labs(x="Lambda",y="MSE")+
   theme(plot.title=element_text(hjust=0.5))+
-  ggtitle("MSE vs Lambda Method Ridge Regression")
+  ggtitle("MSE vs Lambda, Method Ridge Regression")
 
-g.ridge
+print(g.ridge)
 
-#Best lambda value: 0.3764936
+#Best lambda value: 0.3125716
 bestlamb_ridge=cv.ridge$lambda.min
 
 #Estimated MSE
@@ -199,20 +198,21 @@ error.df["RidgeReg","MSE"]=min(cv.ridge$cvm)
 #Final model
 m.ridge_final=glmnet(predictors, response, alpha = 0, lambda = bestlamb_ridge)
 #Estimated coefficients
-coeff.ridge=predict(m.ridge_final, type = "coefficients", s = bestlamb_ridge)[1:8, ]
+coeff.ridge=predict(m.ridge, type = "coefficients", s = bestlamb_ridge)[1:8, ]
 
 df.coeff.estimates[5,]=coeff.ridge
 
 ####Question 1.f####
 
-#Visualizing coefficients values
 m.lasso = glmnet(predictors, response, alpha = 1, lambda = lambdas)
+
+#Visualizing coefficients values vs lambdas
 plot(m.lasso, xvar = "lambda")
 
 #Applying LOOCV to find best lambda
 cv.lasso = cv.glmnet(predictors, response, alpha = 1, nfolds = n, lambda = lambdas, grouped = FALSE, type.measure = "mse")
 
-#Tidy data frames to graph our model
+#Tidy data frames to graph
 tidy_cv <- tidy(cv.lasso)
 glance_cv <- glance(cv.lasso)
 
@@ -222,12 +222,13 @@ g.lasso = ggplot(tidy_cv, aes(lambda, estimate)) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .25)+
   geom_vline(xintercept = glance_cv$lambda.min) +
   geom_vline(xintercept = glance_cv$lambda.1se, lty = 2)+
+  labs(x="Lambda",y="MSE")+
   theme(plot.title=element_text(hjust=0.5))+
-  ggtitle("MSE vs Lambda Method Lasso")
+  ggtitle("MSE vs Lambda, Method Lasso")
 
-g.lasso
+print(g.lasso)
 
-#Best lambda value: 0.1232847
+#Best lambda value: 0.1261857
 bestlamb_lasso=cv.lasso$lambda.min
 
 #Estimated MSE
@@ -244,6 +245,8 @@ df.coeff.estimates[6,]=coeff.lasso
 ####Question 1.g####
 
 final.df=cbind(error.df,df.coeff.estimates)
+
+print(final.df)
 
 #Experiment 2
 print("Experiment 2")
@@ -273,9 +276,9 @@ df.coeff.estimates2=as.data.frame(matrix(NA,nrow=6,ncol = 9))
 
 ####Question 2.a####
 
-#LOOCV on full model of logistic regression
+#10 fold cv on full model of logistic regression
 set.seed(rdseed)
-m.full.loocv = train(
+m.full.cv = train(
   form = Outcome ~ .,
   data = diabetes,
   trControl = train_control,
@@ -283,7 +286,8 @@ m.full.loocv = train(
   family = "binomial"
 )
 
-error.df2["Full","Error"]=1-m.full.loocv$results$Accuracy
+#Estimated test error rate
+error.df2["Full","Error"]=1-m.full.cv$results$Accuracy
 
 #Estimated coefficients
 m.full_glm=glm(Outcome~.,data = diabetes,family = binomial)
@@ -306,16 +310,10 @@ fit.subset.logistic =  bestglm(Xy = diabetes_glm, family = binomial, IC = "AIC",
 
 print(fit.subset.logistic$BestModel$coefficients) #SkinThickness was dropped
 
-#Best model
-m_log.subset=fit.subset.logistic$BestModel 
-#Estimated coefficients
-c.subset=m_log.subset$coefficients
-df.coeff.estimates2[2,-5]=c.subset
 
-
-#LOOCV on best subset model
+#10 fold cv on best subset model
 set.seed(rdseed)
-m.subset.loocv = train(
+m.subset.cv = train(
   form = Outcome ~ .-SkinThickness,
   data = diabetes,
   trControl = train_control,
@@ -323,7 +321,14 @@ m.subset.loocv = train(
   family = "binomial"
 )
 
-error.df2["Subset","Error"]=1-m.subset.loocv$results$Accuracy
+#Estimated test error rate
+error.df2["Subset","Error"]=1-m.subset.cv$results$Accuracy
+
+#Best model
+m_log.subset=glm(Outcome ~ .-SkinThickness,data=diabetes,family = binomial)
+#Estimated coefficients
+c.subset=m_log.subset$coefficients
+df.coeff.estimates2[2,-5]=c.subset
 
 
 ####Question 2.c####
@@ -332,25 +337,14 @@ fit.best.forward =  bestglm(Xy = diabetes_glm, family = binomial, IC = "AIC",
                              method = "forward")
 
 print(fit.best.forward$BestModel$coefficients) #SkinThickness was dropped
+#Same model as in best-subset selection method
 
-#Best model
-m_log.forward=fit.best.forward$BestModel 
+#Estimated test error rate
+error.df2["Forward","Error"]=1-m.subset.cv$results$Accuracy
+
+
 #Estimated coefficients
-c.forward=m_log.forward$coefficients
-df.coeff.estimates2[3,-5]=c.forward
-
-
-#LOOCV on best subset model
-set.seed(rdseed)
-m.forward.loocv = train(
-  form = Outcome ~ .-SkinThickness,
-  data = diabetes,
-  trControl = train_control,
-  method = "glm",
-  family = "binomial"
-)
-
-error.df2["Forward","Error"]=1-m.forward.loocv$results$Accuracy
+df.coeff.estimates2[3,-5]=c.subset
 
 
 
@@ -360,48 +354,57 @@ fit.best.backward =  bestglm(Xy = diabetes_glm, family = binomial, IC = "AIC",
                             method = "backward")
 
 print(fit.best.backward$BestModel$coefficients) #SkinThickness was dropped
+#Same model as best-subset selection method
 
-#Best model
-m_log.backward=fit.best.backward$BestModel 
+#Estimated test error rate
+error.df2["Backward","Error"]=1-m.subset.cv$results$Accuracy
+
 #Estimated coefficients
-c.backward=m_log.backward$coefficients
-df.coeff.estimates2[4,-5]=c.backward
+df.coeff.estimates2[4,-5]=c.subset
 
-
-#LOOCV on best subset model
-set.seed(rdseed)
-m.backward.loocv = train(
-  form = Outcome ~ .-SkinThickness,
-  data = diabetes,
-  trControl = train_control,
-  method = "glm",
-  family = "binomial"
-)
-
-error.df2["Backward","Error"]=1-m.backward.loocv$results$Accuracy
 
 
 
 ####Question 2.e####
 
+#Defining predictors, response and lambdas
 response_diab=diabetes[,9]
 predictors_diab=model.matrix(Outcome ~ ., diabetes)[, -1]
-lambdas = 10^seq(10, -2, length = 100)
-#lambdas = seq(0,0.1,by=0.001)
+lambdas = 10^seq(10, -3, length = 100)
 
-#Visualizing coefficients values
+
+
 mlog.ridge = glmnet(predictors_diab, response_diab, alpha = 0, lambda = lambdas,
                     family = "binomial")
+
+#Visualizing coefficients values vs lambdas
 plot(mlog.ridge, xvar = "lambda")
 
 #Applying 10-fold cv to find best lambda
 set.seed(rdseed)
 cv10.ridge = cv.glmnet(predictors_diab, response_diab, alpha = 0, nfolds = 10, 
                        lambda = lambdas, type.measure = "class", family = "binomial")
-plot(cv10.ridge)
+
+#Tidy data frames to graph
+tidy_cv <- tidy(cv10.ridge)
+glance_cv <- glance(cv10.ridge)
+
+#Plot of MSE as a function of lambda
+g.ridgecv = ggplot(tidy_cv, aes(lambda, estimate)) +
+  geom_point(color="red") + scale_x_log10()+
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .25)+
+  geom_vline(xintercept = glance_cv$lambda.min) +
+  geom_vline(xintercept = glance_cv$lambda.1se, lty = 2)+
+  labs(x="Lambda",y="Test Error Rate")+
+  theme(plot.title=element_text(hjust=0.5))+
+  ggtitle("Test Error vs Lambda, Method Ridge Regression")
+
+print(g.ridgecv)
+
+#Lambda for the minimal test error rate
 loglamb_ridge=cv10.ridge$lambda.min
 
-#Estimated Error
+#Estimated test error
 error.df2["RidgeReg","Error"]=min(cv10.ridge$cvm)
 
 
@@ -417,21 +420,41 @@ df.coeff.estimates2[5,]=c.ridge
 ####Question 2.f####
 
 
-#Visualizing coefficients values
+
 mlog.lasso = glmnet(predictors_diab, response_diab, alpha = 1, lambda = lambdas,
                     family = "binomial")
+
+#Visualizing coefficients values vs lambdas
 plot(mlog.lasso, xvar = "lambda")
 
 #Applying 10-fold cv to find best lambda
 set.seed(rdseed)
 cv10.lasso = cv.glmnet(predictors_diab, response_diab, alpha = 1, nfolds = 10, 
                        lambda = lambdas, type.measure = "class", family = "binomial")
-plot(cv10.lasso)
-loglamb_lasso=cv10.lasso$lambda.min
 
-#Estimated MSE
+#Tidy data frames to graph
+tidy_cv <- tidy(cv10.lasso)
+glance_cv <- glance(cv10.lasso)
+
+#Plot of MSE as a function of lambda
+g.lassocv = ggplot(tidy_cv, aes(lambda, estimate)) +
+  geom_point(color="red") + scale_x_log10()+
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .25)+
+  geom_vline(xintercept = glance_cv$lambda.min) +
+  geom_vline(xintercept = glance_cv$lambda.1se, lty = 2)+
+  labs(x="Lambda",y="Test Error Rate")+
+  theme(plot.title=element_text(hjust=0.5))+
+  ggtitle("Test Error vs Lambda, Method Lasso")
+
+print(g.lassocv)
+
+
+#Estimated test error rate
 error.df2["Lasso","Error"]=min(cv10.lasso$cvm)
 
+
+#Lambda for the minimal test error rate
+loglamb_lasso=cv10.lasso$lambda.min
 
 #Best model
 m_log.lasso=glmnet(predictors_diab, response_diab, alpha = 1, lambda = loglamb_lasso,
